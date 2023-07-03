09eae7983c07105fa8dd80c23ff88576e460cb73
cartilations <alhusseingomma194@gmail.com>
2023-07-03 22:01:23 +0300

    first commit

---
+++ TextPlagiarism/TPProblem.cs
@@ -0,0 +1,359 @@
+﻿using Helpers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Problem
+{
+
+    public class Problem : ProblemBase, IProblem
+    {
+        #region ProblemBase Methods
+        public override string ProblemName { get { return "TextPlagiarism"; } }
+
+        public override void TryMyCode()
+        {
+            int M = 0;
+            int N = 0;
+            int outputVal, expectedVal;
+            string[] subseq;
+
+            {
+                string text1 = "hello world how are you";
+                string text2 = "hello are you world how";
+                expectedVal = 3;
+                outputVal = TextPlagiarism.SolveValue(text1, text2);
+                subseq = TextPlagiarism.ConstructSolution(text1, text2);
+                PrintCase(text1, text2, subseq, outputVal, expectedVal);
+            }
+            {
+                string text1 = "hello world";
+                string text2 = "how are you";
+                expectedVal = 0;
+                outputVal = TextPlagiarism.SolveValue(text1, text2);
+                subseq = TextPlagiarism.ConstructSolution(text1, text2);
+                PrintCase(text1, text2, subseq, outputVal, expectedVal);
+            }
+            {
+                string text1 = "Algorithms Analysis and Design";
+                string text2 = "ANALYSIS & DESIGN of ALGORITHMS \n System analysis and design \n Numerical Analysis \n Data Structures \n S/W DESIGN";
+                expectedVal = 3;
+                outputVal = TextPlagiarism.SolveValue(text1, text2);
+                subseq = TextPlagiarism.ConstructSolution(text1, text2);
+                PrintCase(text1, text2, subseq, outputVal, expectedVal);
+            }
+            {
+                string text1 = "DP is a careful brute force and a complete D&C with overlapped sub-problems";
+                string text2 = "Greedy has two conditions: optimal substructure and safe greedy choice\n\r DP has two conditions: optimal substructure (i.e. D&C) and overlapped subproblems";
+                expectedVal = 3;
+                outputVal = TextPlagiarism.SolveValue(text1, text2);
+                subseq = TextPlagiarism.ConstructSolution(text1, text2);
+                PrintCase(text1, text2, subseq, outputVal, expectedVal);
+            }
+        }
+
+        
+       
+
+        Thread tstCaseThr;
+        bool caseTimedOut ;
+        bool caseException;
+
+        protected override void RunOnSpecificFile(string fileName, HardniessLevel level, int timeOutInMillisec)
+        {
+            int testCases;
+            int M = 0, N = 0;
+            string text1, text2 = null;
+            string path1, path2 = null;
+            int output = -1;
+            int actualResult = -1;
+            int j=0;
+
+            Stream s = new FileStream(fileName, FileMode.Open);
+            StreamReader br = new StreamReader(s);
+
+            testCases = int.Parse(br.ReadLine());
+
+            int totalCases = testCases;
+            int[] correctCases = new int[2];
+            int[] wrongCases = new int[2];
+            int[] timeLimitCases = new int[2];
+            for (int i = 0; i < 2; i++)
+            {
+                correctCases[i] = 0;
+                wrongCases[i] = 0;
+                timeLimitCases[i] = 0;
+            }
+            bool readTimeFromFile = false;
+            if (timeOutInMillisec == -1)
+            {
+                readTimeFromFile = true;
+            }
+            float maxTime = float.MinValue;
+            float avgTime = 0;
+            for (int i = 1; i <= testCases; i++)
+            {
+                path1 = br.ReadLine();
+                path2 = br.ReadLine();
+                actualResult = int.Parse(br.ReadLine());
+                if (readTimeFromFile)
+                {
+                    timeOutInMillisec = int.Parse(br.ReadLine().Split(':')[1]);
+                }
+
+                //if (i < 4)
+                //    continue;
+
+                StreamReader sr = new StreamReader(path1);
+                text1 = sr.ReadToEnd();
+                sr.Close();
+                sr = new StreamReader(path2);
+                text2 = sr.ReadToEnd();
+                sr.Close();
+
+                Console.WriteLine("\n===========================");
+                Console.WriteLine("CASE#{0}:", i);
+                Console.WriteLine("===========================");
+
+                for (int c = 0; c < 2; c++)
+                {
+                    caseTimedOut = true;
+                    Stopwatch sw = null;
+                    caseException = false;
+                    string[] outputVals = null;
+                    {
+                        tstCaseThr = new Thread(() =>
+                        {
+                            try
+                            {
+                                sw = Stopwatch.StartNew();
+                                if (c == 0)
+                                {
+                                    output = TextPlagiarism.SolveValue(text1, text2);
+                                }
+                                else
+                                {
+                                    outputVals = TextPlagiarism.ConstructSolution(text1, text2);
+                                    if (outputVals == null)
+                                        output = 0;
+                                }
+                                sw.Stop();
+                                Console.WriteLine("time = {0} ms", sw.ElapsedMilliseconds);
+                                Console.WriteLine("output = {0}", output);
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                                caseException = true;
+                                output = -1;
+                                outputVals = null;
+                            }
+                            caseTimedOut = false;
+                        });
+
+                        //StartTimer(timeOutInMillisec);
+                        tstCaseThr.Start();
+                        tstCaseThr.Join(timeOutInMillisec);
+                    }
+
+                    if (caseTimedOut)       //Timedout
+                    {
+                        tstCaseThr.Abort();
+                        Console.WriteLine("Time Limit Exceeded in Case {0} [FUNCTION#{1}].", i, c+1);
+                        timeLimitCases[c]++;
+                    }
+                    else if (caseException) //Exception 
+                    {
+                        Console.WriteLine("Exception in Case {0} [FUNCTION#{1}].", i, c+1);
+                        wrongCases[c]++;
+                    }
+                    else if (output == actualResult)    //Passed
+                    {
+                        if (c == 0)
+                        {
+                            Console.WriteLine("Test Case {0} [FUNCTION#{1}] Passed!", i, c + 1);
+                            correctCases[c]++;
+                        }
+                        else if (CheckOutput(text1, text2, outputVals, output, actualResult))
+                        {
+                            Console.WriteLine("Test Case {0} [FUNCTION#{1}] Passed!", i, c + 1);
+                            correctCases[c]++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong Answer in Case {0} [FUNCTION#{1}].", i, c + 1);
+                            wrongCases[c]++;
+                        }
+                        //maxTime = Math.Max(maxTime, sw.ElapsedMilliseconds);
+                        //avgTime += sw.ElapsedMilliseconds;
+                    }
+                    else                    //WrongAnswer
+                    {
+                        Console.WriteLine("Wrong Answer in Case {0} [FUNCTION#{1}].", i, c+1);
+                        if (level == HardniessLevel.Easy)
+                        {
+                            PrintCase(text1, text2, outputVals, output, actualResult, false);
+                        }
+                        wrongCases[c]++;
+                    }
+                }
+            }
+            s.Close();
+            br.Close();
+            for (int c = 0; c < 2; c++)
+            {
+                Console.WriteLine("EVALUATION OF FUNCTION#{0}:", c+1);
+                Console.WriteLine("# correct = {0}", correctCases[c]);
+                Console.WriteLine("# time limit = {0}", timeLimitCases[c]);
+                Console.WriteLine("# wrong = {0}", wrongCases[c]);
+                //Console.WriteLine("\nFINAL EVALUATION (%) = {0}", Math.Round((float)correctCases / totalCases * 100, 0));
+                //Console.WriteLine("AVERAGE EXECUTION TIME (ms) = {0}", Math.Round(avgTime / (float)correctCases, 2));
+                //Console.WriteLine("MAX EXECUTION TIME (ms) = {0}", maxTime); 
+            }
+            Console.WriteLine("\nFINAL EVALUATION: FUNCTION#1 (%), FUNCTION#2 (%) = {0} {1}", Math.Round((float)correctCases[0] / totalCases * 100, 0), Math.Round((float)correctCases[1] / totalCases * 100, 0));
+                
+        }
+
+       
+
+        protected override void OnTimeOut(DateTime signalTime)
+        {
+        }
+
+        public override void GenerateTestCases(HardniessLevel level, int numOfCases, bool includeTimeInFile = false, float timeFactor = 1)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Helper Methods
+        private static void PrintVals(string[] X)
+        {
+            int N = X.Length;
+            for (int i = 0 ; i < N; i++)
+            {
+                    Console.Write(X[i] + "  ");
+            }
+            Console.WriteLine();
+        }
+
+        private void PrintCase(string text1, string text2, string[] subseq, int outputVal, int expectedVal, bool check = true)
+        {
+            Console.WriteLine("text1 = {0}", text1);
+            Console.WriteLine("text2 = {0}", text2);
+            Console.WriteLine("expected value = {0}", expectedVal);
+            Console.WriteLine("output value = {0}", outputVal);
+            if (subseq != null)
+            {
+                Console.Write("output subseq = "); PrintVals(subseq);
+            }
+
+            if (check)
+            {
+                if (outputVal != expectedVal)
+                {
+                    Console.WriteLine("WRONG");
+                }
+                else
+                {
+                    if (CheckOutput(text1, text2, subseq, outputVal, expectedVal)) Console.WriteLine("CORRECT");
+                    else Console.WriteLine("WRONG");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        private bool CheckOutput(string paragraph, string text, string[] subseq, int output, int expectedResult)
+        {
+            if (subseq == null)
+            {
+                if (expectedResult > 0)
+                    return false;
+                else
+                    return true;
+            }
+
+            if (output != expectedResult)
+            {
+                Console.WriteLine("WRONG");
+                return false;
+            }
+
+            if (subseq.Length != expectedResult)
+            {
+                Console.WriteLine("WRONG");
+                return false;
+            }
+            paragraph = paragraph.ToLower();
+            text = text.ToLower();
+            
+            char[] paragraphSeperators = { '\n', '\r' };
+            string[] paragraphs = text.Split(paragraphSeperators, StringSplitOptions.RemoveEmptyEntries);
+            
+            char[] wordSeperators = { ' ', '\t' };
+            string[] w1 = paragraph.Split(wordSeperators, StringSplitOptions.RemoveEmptyEntries);
+
+            bool found = false;
+            for (int p = 0; p < paragraphs.Length; p++)
+            {
+                string[] w2 = paragraphs[p].Split(wordSeperators, StringSplitOptions.RemoveEmptyEntries);
+                found = CheckWordsOccurance(subseq, w1, w2);
+                if (found) break;
+            }
+            if (!found)
+            {
+                Console.WriteLine("WRONG: Invalid subsequence in paragraph or text! one or more word is not found or not in a correct subseq order");
+                return false;
+            }
+                
+            return true;
+        }
+
+        private bool CheckWordsOccurance(string[] subseq, string[] w1, string[] w2)
+        {
+            int j1 = 0, j2 = 0;
+            for (int i = 0; i < subseq.Length; i++)
+            {
+                bool found = false;
+                for (; j1 < w1.Length; j1++)
+                {
+                    if (w1[j1] == subseq[i].ToLower())
+                    {
+                        found = true;
+                        j1++;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    return false;
+                }
+                found = false;
+                for (; j2 < w2.Length; j2++)
+                {
+                    if (w2[j2] == subseq[i].ToLower())
+                    {
+                        found = true;
+                        j2++;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        #endregion
+   
+    }
+}
---
+++ TextPlagiarism/TextPlagiarism.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Split the given text into paragraphs using the paragraph separator ('\n' or '\r').
+Split the given paragraph and each paragraph in the text into lowercase words using word separators (' ' or '\t').
+Initialize a variable to store the maximum common subsequence of words and set it to 0.
+Initialize a list to store the common words in the maximum common subsequence.
+Iterate through each paragraph in the text.
+For each paragraph, perform the Longest Common Subsequence (LCS) algorithm between the words in the given paragraph and the words in the current text paragraph.
+Update the maximum common subsequence length if the LCS result is greater than the current maximum.
+If the maximum common subsequence length is updated, clear the list of common words and reconstruct the common subsequence by iterating through the LCS matrix.
+Return the maximum common subsequence length as the plagiarism similarity value.
+To implement the two required functions:
+
+The first function should take the given paragraph and the complete text as input. 
+It should internally follow the steps above and return the plagiarism similarity value.
+
+The second function should take the given paragraph and the complete text as input.
+It should also follow the steps above, but instead of returning the plagiarism similarity value,
+it should return the subsequence of common words (if any) between the given paragraph and the paragraph in the text with the maximum common subsequence. If there is no common subsequence, the function should return null.
+*/
+namespace Problem
+{
+
+    public static class TextPlagiarism
+    {
+        // Calculates the maximum similarity between a given paragraph and a text
+        public static int SolveValue(string paragraph, string text)
+        {
+            // Split the given paragraph into words
+            string[] paragraphWords = SplitIntoWords(paragraph);
+
+            // Split the complete text into paragraphs
+            string[] textParagraphs = SplitIntoParagraphs(text);
+
+            // Initialize variable to store the maximum similarity
+            int maxSimilarity = 0;
+
+            // Iterate through each paragraph in the text
+            foreach (string textParagraph in textParagraphs)
+            {
+                // Split the current text paragraph into words
+                string[] textParagraphWords = SplitIntoWords(textParagraph);
+
+                // Calculate the similarity between the given paragraph and the current text paragraph
+                int similarity = CalculateSimilarity(paragraphWords, textParagraphWords);
+
+                // Update the maximum similarity if the current similarity is greater
+                maxSimilarity = Math.Max(maxSimilarity, similarity);
+            }
+
+            // Return the maximum similarity value
+            return maxSimilarity;
+        }
+
+        // Constructs a solution by finding common words between a given paragraph and a text
+        public static string[] ConstructSolution(string paragraph, string text)
+        {
+            string[] wordsInText = WordsFromText(text);
+            string[] wordsInParagraph = WordsFromParagraph(paragraph);
+
+            // Initialize variables
+            int maxCommonSubsequence = 0;
+            List<string> commonWords = new List<string>();
+
+            // Iterate through each word in the text
+            foreach (string word in wordsInText)
+            {
+                // Split the current word into lowercase words
+                string[] wordWords = word.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+                // Create a matrix to store the longest common subsequence lengths
+                int[,] longestCommonSubsequence = new int[wordsInParagraph.Length + 1, wordWords.Length + 1];
+
+                // Perform longest common subsequence algorithm
+                LongestCommonSubs(wordsInParagraph, wordWords, longestCommonSubsequence);
+
+                // Check if the current longest common subsequence is greater than the previous maximum
+                maxCommonSubsequence = Cheaker(wordsInParagraph, maxCommonSubsequence, commonWords, wordWords, longestCommonSubsequence);
+            }
+
+            return commonWords.ToArray();
+        }
+
+        // Splits a paragraph into an array of lowercase words
+        private static string[] WordsFromParagraph(string paragraph)
+        {
+            return paragraph.ToLower().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        // Splits a text into an array of lowercase words
+        private static string[] WordsFromText(string text)
+        {
+            // Split the input text and paragraph into lowercase words
+            return text.ToLower().Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        // Updates the maximum common subsequence and reconstructs the common words list
+        private static int Cheaker(string[] wordsInParagraph, int maxCommonSubsequence, List<string> commonWords, string[] wordWords, int[,] longestCommonSubsequence)
+        {
+            if (longestCommonSubsequence[wordsInParagraph.Length, wordWords.Length] > maxCommonSubsequence)
+            {
+                // Update the maximum common subsequence length and clear the list of common words
+                maxCommonSubsequence = longestCommonSubsequence[wordsInParagraph.Length, wordWords.Length];
+                commonWords.Clear();
+
+                // Reconstruct the common subsequence
+                ReconstructCommonSubs(wordsInParagraph, commonWords, wordWords, longestCommonSubsequence);
+
+                // Reverse the list of common words
+                commonWords.Reverse();
+            }
+
+            return maxCommonSubsequence;
+        }
+
+        // Reconstructs the common subsequence of words
+        private static void ReconstructCommonSubs(string[] wordsInParagraph, List<string> commonWords, string[] wordWords, int[,] longestCommonSubsequence)
+        {
+            int p = wordsInParagraph.Length;
+            int h = wordWords.Length;
+            while (p > 0 && h > 0)
+            {
+                if (wordsInParagraph[p - 1] == wordWords[h - 1])
+                {
+                    commonWords.Add(wordsInParagraph[p - 1]);
+                    p--;
+                    h--;
+                }
+                else if (longestCommonSubsequence[p - 1, h] > longestCommonSubsequence[p, h - 1])
+                    p--;
+                else
+                    h--;
+            }
+        }
+
+        // Performs the longest common subsequence algorithm
+        private static void LongestCommonSubs(string[] wordsInParagraph, string[] wordWords, int[,] longestCommonSubsequence)
+        {
+            for (int i = 0; i <= wordsInParagraph.Length; i++)
+            {
+                for (int j = 0; j <= wordWords.Length; j++)
+                {
+                    if (i == 0 || j == 0)
+                        longestCommonSubsequence[i, j] = 0;
+                    else if (wordsInParagraph[i - 1] == wordWords[j - 1])
+                        longestCommonSubsequence[i, j] = longestCommonSubsequence[i - 1, j - 1] + 1;
+                    else
+                        longestCommonSubsequence[i, j] = Math.Max(longestCommonSubsequence[i - 1, j], longestCommonSubsequence[i, j - 1]);
+                }
+            }
+        }
+
+        // Splits a string into an array of lowercase words, removing empty entries
+        private static string[] SplitIntoWords(string input)
+        {
+            // Split the input into words, removing empty entries
+            return input.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                        .Select(word => word.ToLower())
+                        .ToArray();
+        }
+
+        // Splits a string into an array of paragraphs, removing empty entries
+        private static string[] SplitIntoParagraphs(string input)
+        {
+            // Split the input into paragraphs, removing empty entries
+            return input.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        // Calculates the similarity between two arrays of words
+        private static int CalculateSimilarity(string[] words1, string[] words2)
+        {
+            // Create a matrix to store the similarity values
+            int[,] similarityMatrix = new int[words1.Length + 1, words2.Length + 1];
+
+            // Iterate through the words of the first sequence
+            for (int i = 1; i <= words1.Length; i++)
+            {
+                // Iterate through the words of the second sequence
+                for (int j = 1; j <= words2.Length; j++)
+                {
+                    // Compare the current words for equality, ignoring case
+                    if (string.Equals(words1[i - 1], words2[j - 1], StringComparison.OrdinalIgnoreCase))
+                    {
+                        // If the words are equal, increment the similarity value
+                        similarityMatrix[i, j] = similarityMatrix[i - 1, j - 1] + 1;
+                    }
+                    else
+                    {
+                        // If the words are not equal, take the maximum similarity value from the previous positions
+                        similarityMatrix[i, j] = Math.Max(similarityMatrix[i - 1, j], similarityMatrix[i, j - 1]);
+                    }
+                }
+            }
+
+            // Return the final similarity value
+            return similarityMatrix[words1.Length, words2.Length];
+        }
+
+    }
+}
